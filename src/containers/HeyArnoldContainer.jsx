548764bae93a197df37098499efd53b90dc73040
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-09-08 17:24:15 -0700

    added default state to container class component

--- src/containers/HeyArnoldContainer.jsx
+++ src/containers/HeyArnoldContainer.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 export default class HeyArnoldContainer extends Component {
+  state = {
+    loading: true,
+    characters: []
+  };
+
   render() {
     return <h1>Loading...</h1>;
   }
