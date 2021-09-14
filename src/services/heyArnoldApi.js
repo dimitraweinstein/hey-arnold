6f68b565213f55d51e10f3545bf4f1017942a63e
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-09-13 20:14:04 -0700

    changed service functions to async-await syntax

--- src/services/heyArnoldApi.js
+++ src/services/heyArnoldApi.js
@@ -1,21 +1,25 @@
-export const findCharacters = () => {
-  return fetch('https://hey-arnold-api.herokuapp.com/api/v1/characters')
-    .then((res) => res.json())
-    .then((json) =>
-      json.map((character) => ({
-        id: character.id,
-        name: character.name,
-        image: character.image
-      }))
-    );
+export const findCharacters = async () => {
+  // eslint-disable-next-line max-len
+  const res = await fetch('https://hey-arnold-api.herokuapp.com/api/v1/characters');
+  const json = await res.json();
+    
+  return json.results.map((character) => ({
+    id: character.id,
+    name: character.name,
+    image: character.image
+  }));
 };
 
-export const findCharacterById = (id) => {
-  return fetch(`https://hey-arnold-api.herokuapp.com/api/v1/characters/${id}`)
-    .then((res) => res.json())
-    .then((json) => ({
-      id: json._id,
-      name: json.name,
-      image: json.image
-    }));
+export const findCharacterById = async (id) => {
+  // eslint-disable-next-line max-len
+  const res = await fetch(`https://hey-arnold-api.herokuapp.com/api/v1/characters/${id}`);
+  const json = await res.json();
+      
+  const character = {
+    id: json._id,
+    name: json.name,
+    image: json.image
+  };
+
+  return character;
 };
